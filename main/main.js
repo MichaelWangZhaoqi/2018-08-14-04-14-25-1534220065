7c300ae10ae64b3063771367f36ef7a04de4e07b
unknown <118020910379@sjtu.edu.cn>
2019-05-30 22:20:24 +0800

    First Virsion

--- main/main.js
+++ main/main.js
@@ -1,4 +1,36 @@
-module.exports = function main() {
+module.exports = function main(str) {
     console.log("Debug Info");
+	var firstline=["._.", "...", "._.", "._.", "...", "._.", "._.", "._.", "._.", "._."];
+	var secondline=["|.|", "..|", "._|", "._|", "|_|", "|_.", "|_.", "..|", "|_|", "|_|"];
+	var thirdline=["|_|", "..|", "|_.", "._|", "..|", "._|", "|_|", "..|", "|_|", "..|"];
+	var len=str.length;
+	
+	for (var i=0; i<len; i++){
+		var thisnum=parseInt(str[i]);
+		console.log(firstline[thisnum]);
+		if (i!=len-1){
+			console.log(" ");
+		}
+	}
+		
+	for (var i=0; i<len; i++){
+		var thisnum=parseInt(str[i]);
+		console.log(secondline[thisnum]);
+		console.log(" ");
+		if (i!=len-1){
+			console.log(" ");
+		}
+	}
+	
+	for (var i=0; i<len; i++){
+		var thisnum=parseInt(str[i]);
+		console.log(thirdline[thisnum]);
+		console.log(" ");
+		if (i!=len-1){
+			console.log(" ");
+		}
+	}
+	
+		
     return 'Hello World!';
 };
